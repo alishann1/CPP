bd29246b8fc603f4260ac9a6883d3dc017fbc35b
Ali Shan <alishanamin125@gmail.com>
2025-09-16 20:50:11 +0500

    init

---
+++ practice1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    string food = "Pizza";
+    string &meal = food;
+
+    cout << food << '\n';
+    cout << meal << '\n';
+
+    meal = "Pasta";
+    cout << meal << '\n';
+    cout << food;
+    return 0;
+}
