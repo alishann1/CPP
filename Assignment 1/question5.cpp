267f9187e8222fe4086d553144995abb8f53f9b4
Ali Shan <alishanamin125@gmail.com>
2025-10-25 15:02:35 +0500

    init

--- Assignment 1/question5.cpp
+++ Assignment 1/question5.cpp
@@ -4,16 +4,21 @@ using namespace std;
 int main()
 {
     int n;
+    cout << "Enter size of array: ";
     cin >> n;
 
     int *arr = new int[n];
+    cout << "Enter " << n << " elements: ";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
     int x, pos;
-    cin >> x >> pos;
+    cout << "Enter element to insert: ";
+    cin >> x;
+    cout << "Enter position (0-based index): ";
+    cin >> pos;
 
     int *newArr = new int[n + 1];
 
@@ -29,10 +34,12 @@ int main()
         newArr[i + 1] = arr[i];
     }
 
+    cout << "Updated array: ";
     for (int i = 0; i < n + 1; i++)
     {
         cout << newArr[i] << " ";
     }
+    cout << endl;
 
     delete[] arr;
     delete[] newArr;
