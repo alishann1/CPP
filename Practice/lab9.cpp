ba60b9545e2cbe2d688125c6185e9f7dd3086573
Ali Shan <alishanamin125@gmail.com>
2025-11-12 12:10:25 +0500

    lab9

--- Practice/lab9.cpp
+++ Practice/lab9.cpp
@@ -21,4 +21,9 @@ int main()
     TreeNode *secondNode = new TreeNode(3);
     TreeNode *thirdNode = new TreeNode(4);
     TreeNode *fourthNode = new TreeNode(5);
+
+    firstNode->left = secondNode;
+    firstNode->right = thirdNode;
+    secondNode->left = fourthNode;
+    return 0;
 }
